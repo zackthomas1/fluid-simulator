18479a536cc03db93e9a4516bbff1985d8b8740c
zackthomas1 <zackthomas13@gmail.com>
2023-11-16 11:20:10 -0800

    Draw first triangle

--- FluidSimulator/src/Application.cpp
+++ FluidSimulator/src/Application.cpp
@@ -3,6 +3,21 @@
 
 #include <iostream>
 
+// shader programs
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"gl_Position=vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"FragColor = vec4(1.0f,0.5f,0.2f,1.0f);\n"
+"}\0";
+
 //
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
@@ -44,6 +59,74 @@ int main(int argc, char** argv)
 
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+	//set up vertex data (and buffer(s)) and configure vertex attributes
+	// ---------------------------------------------------
+	float vertices[] = {
+		-0.5f, -0.5f, 0.0f,
+		 0.5f, -0.5f, 0.0f,
+		 0.0f,  0.5f, 0.0f
+	};
+
+	// bind Vertex Array Object
+	unsigned int VBO, VAO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO); 
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO); 
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	// set vertex attributes pointers
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+
+	// build and complie shader program
+	// ---------------------------------------------------
+	// vertex shader
+	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER); 
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader); 
+
+	int success;
+	char infoLog[512]; 
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success); 
+	if (!success) {
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" <<
+			infoLog << std::endl;
+	}
+
+	//  fragment shader 
+	unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER); 
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL); 
+	glCompileShader(fragmentShader); 
+
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success); 
+	if (!success) {
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << "ERROR:SHADER::FRAGMENT::COMPILATION_FAILED\n" <<
+			infoLog << std::endl;
+	}
+
+	// link shaders
+	unsigned int shaderProgram = glCreateProgram(); 
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success); 
+	if (!success) {
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "ERROR::PROGRAM::LINKING_FAILED\n" <<
+			infoLog << std::endl;
+	}
+
+	// delte shader objects once linked with a program
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+
 	// render loop
 	// -----------
 	while (!glfwWindowShouldClose(window))
@@ -54,6 +137,11 @@ int main(int argc, char** argv)
 		// rendering commands here
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);	// state-setting
 		glClear(GL_COLOR_BUFFER_BIT);			// state-using
+
+		// draw triangle
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		// glfw: swap buffers and poll IO events (key pressed/released, mouse moved)
 		// check and call events and swap the buffers
